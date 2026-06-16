f89a28f6f9badd955f66e4f45fd180f89dd02d93
jsmit1993 <156153441+jsmit1993@users.noreply.github.com>
2026-06-16 16:20:09 -0400

    Correct box functions
    Old functions only created a 1xnumber @ set width rather than a equal sided box.
    Now working on creating the correct flexbox updates to make it equal sided.

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -2,13 +2,29 @@ let grid = document.querySelector('.etch-a-sketch-div');
 
 function createBoxGrid(size) {
         grid.replaceChildren();
-        for (i = 0; i <= size; i++) {
+        let newSquareSize = 500 / size;
+        for (i = 0; i <= size * size; i++) {
             let newBox = document.createElement('div');
             newBox.classList.add('newBox'); 
+            newBox.style.width = `${newSquareSize}`;
+            newBox.style.height = `${newSquareSize}`;
             grid.appendChild(newBox);
         }
      
 }
+
+let squareSize16 = 500 / 16;
+function createBoxesonLoad() {
+    for (i = 0; i < 16 * 16; i++) {
+        let newBox = document.createElement('div');
+        newBox.classList.add('newBox'); 
+        newBox.style.width = `${squareSize16}`;
+        newBox.style.height = `${squareSize16}`;
+        grid.appendChild(newBox);
+        console.log("number of boxes made is 16");
+    }
+}
+createBoxesonLoad()
 
 let createNewGrid = document.querySelector('.createNewGrid')
 let gridSize = document.querySelector('.newBoxSize');
